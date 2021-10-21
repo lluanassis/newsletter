7f8ae0e22bc3695bb0429fe86706eb9cf881534b
Luan Assis <luan2lobo@gmail.com>
2021-10-20 22:33:07 -0300

    páginas sucesso e falha

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html")
 })
@@ -39,6 +40,13 @@ app.post("/", function(req, res){
     }
 
     const request = https.request(url, options, function(response){
+        
+        if (response.statusCode = 200){
+            res.sendFile(__dirname + "/success.html")
+        } else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+        
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -49,7 +57,9 @@ app.post("/", function(req, res){
 
 })
 
-
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
 
 
 
